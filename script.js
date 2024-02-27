f898e6d89721d69f340af70e6ddbe0d628b04b48
AlexisGomez <wilaleva2019@gmail.com>
2024-02-27 14:08:28 -0400

    Proyecto casi completo

---
+++ script.js
@@ -0,0 +1,93 @@
+let inputPalabra = document.getElementById("palabra");
+let intentar = document.getElementById("intentar");
+let divResultados = document.getElementById("resultados");
+let resultadoH2 = document.getElementById("resultadoH2");
+let perdiste = document.getElementById("perdiste");
+let textDiv = document.getElementsByClassName("textDiv");
+let resultadosPalabras = document.getElementById("palabrasRes");
+let reiniciar = document.getElementById("reiniciar");
+let error = document.getElementById("error");
+let ganaste = document.getElementById("ganaste");
+
+let palabraCorrecta = "BUENO";
+let palabraCorrArr = palabraCorrecta.split("");
+let intentos = 0;
+
+const agregaIntento = () => {
+    resultadoH2.style.display = "none";
+    intentos += 1;
+
+    if(intentos <= 6){
+        let palabra = inputPalabra.value;
+        
+        if(palabra.length == 5){
+            error.style.display = "none";
+            let palabraMayus = palabra.toUpperCase()
+
+            if(palabraMayus == palabraCorrecta){
+                gana();
+            }
+
+            let palabraArr = palabraMayus.split("");
+            let divText = document.createElement("div");
+            divText.classList.add("textDiv");
+        
+            for(let i = 0; i < palabraArr.length; i++){
+                let pText = document.createElement("p");
+                pText.classList.add("text");
+        
+                if(palabraArr[i] == palabraCorrArr[i]){
+                    pText.classList.add("correcto");
+                }else if(palabraCorrArr.includes(palabraArr[i])){
+                    pText.classList.add("contiene");
+                }
+        
+                pText.textContent = palabraArr[i];
+                divText.appendChild(pText);
+            }
+        
+            resultadosPalabras.appendChild(divText);
+        }else{
+            error.style.display = "block";
+        }
+    }else{
+        perdio();
+    }
+}
+
+const perdio = () => {
+    resultadosPalabras.innerHTML = "";
+    intentar.style.pointerEvents = "none";
+    intentar.style.opacity = 0.5;
+    perdiste.style.display = "block";
+    reiniciar.style.pointerEvents = "auto";
+    reiniciar.style.opacity = 1;
+}
+
+const reinicio = () => {
+    intentos = 0;
+    reiniciar.style.pointerEvents = "none";
+    reiniciar.style.opacity = 0.5;
+    perdiste.style.display = "none";
+    resultadoH2.style.display = "block";
+    intentar.style.pointerEvents = "auto";
+    intentar.style.opacity = 1;
+    ganaste.style.display = "none";
+    resultadosPalabras.innerHTML = "";
+}
+
+const gana = () => {
+    ganaste.style.display = "block";
+    intentar.style.pointerEvents = "none";
+    intentar.style.opacity = 0.5;
+    reiniciar.style.pointerEvents = "auto";
+    reiniciar.style.opacity = 1;
+}
+
+intentar.addEventListener("click", agregaIntento)
+
+inputPalabra.addEventListener("keypress", (e) => {
+    if (e.key == "Enter") agregaIntento();
+});
+
+reiniciar.addEventListener("click", reinicio)
